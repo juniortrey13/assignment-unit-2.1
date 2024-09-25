cd2c77b0f47d7551cbbfeb2019d6538060158e54
Junior Avalos <junior.trey.avalos13@gmail.com>
2024-09-25 14:00:53 -0500

    corrected variable for number variable

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -12,7 +12,7 @@ let fullName = 'Junior Avalos';
 console.log(fullName);
 // 4 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
 //     Console log the value of `luckyNumber`
-let luckyNumber = '13'
+luckyNumber = 13;
 console.log(luckyNumber);
 // 5 - Create a variable `introduction` and using the variables from above,
 //     give it the value of:
