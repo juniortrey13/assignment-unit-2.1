be6c0b5cab1c9f7d34c642c54a5a89ede208f888
Junior Avalos <junior.trey.avalos13@gmail.com>
2024-09-25 14:48:19 -0500

    I tried to asssign 'Result', 'Dice Roll', and 'Pet Status' to a String but I don't think I did it right

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -45,13 +45,24 @@ console.log(allowedPets);
 //      if adventurous is true, set `result` to be "Adventures are great!", 
 //      if it's not true,  set `result` to be "How about we stay home?"
 //      Console log the value of `result`
-
+let result = adventurous;
+if (adventurous == true){
+  console.log("Adventures are great!");
+}
+else{
+  console.log("How about we stay home?")
+}
+console.log(result);
 // 13 - Create a variable called `diceRoll` and set it to the value of "Try again later.".
 //      Create a compound conditional: 
 //      if luckyNumber is 2 and adventurous is true,
 //      set `diceRoll` to be "Roll the dice!"
 //      Console log the value of `diceRoll`
-
+let diceRoll = 'Try again later';
+if(this.luckyNumber === 2, adventurous === true){
+  console.log("Roll the dice!");
+}
+console.log(diceRoll);
 // 14 - Create a variable called `petStatus`. 
 //      Write a conditional that covers the following:
 //      if the value of `pets` is less than the value of `allowedPets`,
@@ -60,7 +71,17 @@ console.log(allowedPets);
 //      set `petStatus` to the value of "I have enough pets" 
 //      if the value of `pets` is greater than the value of `allowedPets`.
 //      set `petStatus` to the value of "Oh no, I have too many pets!"
-
+let petStatus = allowedPets;
+if (pets < allowedPets){
+  console.log('I can have more pets');
+}
+if (this.pets = allowedPets){
+  console.log('I have enough pets');
+}
+if (pets > allowedPets){
+  console.log("Oh no, I have too many pets!");
+}
+console.log(petStatus);
 
 // STRETCH GOALS:
 
