ba3d08eadd7db5ca0d158aceba2d059d22d56f08
Junior Avalos <junior.trey.avalos13@gmail.com>
2024-09-25 13:53:15 -0500

    set variable for 'fullName'

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -8,7 +8,8 @@ console.log(lastName);
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 //     (remember, you already have variables for this, can you use those?)
 //     Console log the value of `fullName`
-
+let fullName = 'Junior Avalos';
+console.log(fullName);
 // 4 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
 //     Console log the value of `luckyNumber`
 
